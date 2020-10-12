754310e4492300a3c1f043ade48c95b6368d7d99
Abhishek Kumar <abhishek022kk@gmail.com>
2020-10-12 07:10:35 +0530

    4 ques done

--- Watermelon.cpp
+++ Watermelon.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    cin >> t;
-    while(t--){
-        cout << "Abhishek\n";
-    }
+    int n;
+    cin >> n;
+    if(n%2==0 && n>2)
+        cout << "YES";
+    else
+        cout << "NO";
 }
