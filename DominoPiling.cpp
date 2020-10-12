433def501a26b68e225366870cda457e9d186a4b
Abhishek Kumar <abhishek022kk@gmail.com>
2020-10-12 13:06:27 +0530

    6 ques done

--- DominoPiling.cpp
+++ DominoPiling.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    cin >> t;
-    while(t--){
-        
-    }
+    int m, n;
+    cin >> m >> n;
+    cout << (m*n)/2;
 }
